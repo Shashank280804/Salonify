fcf44e248c6d740832dc38cc80eb944946486767
shashank <116015929+Shashank280804@users.noreply.github.com>
2025-11-23 18:28:56 +0530

    Added controller layer and dtos,mappers to the booking microservice

--- Microservices/booking-microservice/src/main/java/com/salonify/booking_microservice/controller/BookingController.java
+++ Microservices/booking-microservice/src/main/java/com/salonify/booking_microservice/controller/BookingController.java
@@ -1,16 +1,17 @@
 package com.salonify.booking_microservice.controller;
 
-import com.salonify.booking_microservice.dto.BookingRequest;
-import com.salonify.booking_microservice.dto.SalonDTO;
-import com.salonify.booking_microservice.dto.UserDTO;
+import com.salonify.booking_microservice.dto.*;
+import com.salonify.booking_microservice.mapper.BookingMapper;
 import com.salonify.booking_microservice.model.Booking;
 import com.salonify.booking_microservice.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -19,17 +20,62 @@ public class BookingController {
 
     private final BookingService bookingService;
 
-    public ResponseEntity<Booking> createBooking(@RequestParam Long salonId, @RequestBody BookingRequest bookingRequest){
+    @PostMapping
+    public ResponseEntity<Booking> createBooking(@RequestParam Long salonId, @RequestBody BookingRequest bookingRequest) throws Exception {
 
         UserDTO user = new UserDTO();
-
         user.setId(1L);
 
         SalonDTO salon = new SalonDTO();
         salon.setId(salonId);
 
-        return null;
+        Set<ServiceDTO> serviceDTOSet = new HashSet<>();
 
+        ServiceDTO serviceDTO = new ServiceDTO();
+        serviceDTO.setId(1L);
+        serviceDTO.setPrice(399);
+        serviceDTO.setDuration(45);
+        serviceDTO.setName("Haircut for men");
+
+        Booking booking = bookingService.createBooking(bookingRequest,user,salon,serviceDTOSet);
+
+
+        return ResponseEntity.ok(booking);
+
+    }
+
+    @GetMapping("/customer")
+    public ResponseEntity<Set<BookingDTO>> getBookingsByCustomer(){
+
+
+        List<Booking> bookings = bookingService.getBookingsByCustomer(1L);
+
+        return ResponseEntity.ok(getBookingDTOs(bookings));
+    }
+
+    private Set<BookingDTO> getBookingDTOs(List<Booking> bookings){
+        return bookings.stream().map(booking -> {
+            return BookingMapper.toDTO(booking);
+        }).collect(Collectors.toSet());
+
+    }
+
+    @GetMapping("/salon")
+    public ResponseEntity<Set<BookingDTO>> getBookingsBySalon(){
+
+
+        List<Booking> bookings = bookingService.getBookingsBySalon(1L);
+
+        return ResponseEntity.ok(getBookingDTOs(bookings));
+    }
+
+    @GetMapping("/{bookingId}")
+    public ResponseEntity<Set<BookingDTO>> getBookingsById(){
+
+
+        List<Booking> bookings = bookingService.getBookingsBySalon(1L);
+
+        return ResponseEntity.ok(getBookingDTOs(bookings));
     }
 
 }
---
+++ Microservices/booking-microservice/src/main/java/com/salonify/booking_microservice/mapper/BookingMapper.java
@@ -0,0 +1,21 @@
+package com.salonify.booking_microservice.mapper;
+
+import com.salonify.booking_microservice.dto.BookingDTO;
+import com.salonify.booking_microservice.model.Booking;
+
+public class BookingMapper {
+
+    public static BookingDTO toDTO(Booking booking){
+        BookingDTO bookingDTO = new BookingDTO();
+        bookingDTO.setId(booking.getId());
+        bookingDTO.setCustomerId(booking.getCustomerId());
+        bookingDTO.setStatus(booking.getStatus());
+        bookingDTO.setEndTime(booking.getEndTime());
+        bookingDTO.setStartTime(booking.getStartTime());
+        bookingDTO.setSalonId(booking.getSalonId());
+        bookingDTO.setServiceIds(booking.getServiceIds());
+
+        return bookingDTO;
+
+    }
+}
