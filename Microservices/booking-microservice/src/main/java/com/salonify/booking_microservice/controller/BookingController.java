3f530be70c0b30fee9f84fb6aa855ebb29a3ce66
shashank <116015929+Shashank280804@users.noreply.github.com>
2025-11-23 18:46:18 +0530

    Completed controller layer, added all the endpoints

--- Microservices/booking-microservice/src/main/java/com/salonify/booking_microservice/controller/BookingController.java
+++ Microservices/booking-microservice/src/main/java/com/salonify/booking_microservice/controller/BookingController.java
@@ -1,13 +1,16 @@
 package com.salonify.booking_microservice.controller;
 
+import com.salonify.booking_microservice.domain.BookingStatus;
 import com.salonify.booking_microservice.dto.*;
 import com.salonify.booking_microservice.mapper.BookingMapper;
 import com.salonify.booking_microservice.model.Booking;
+import com.salonify.booking_microservice.model.SalonReport;
 import com.salonify.booking_microservice.service.BookingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -70,12 +73,46 @@ public class BookingController {
     }
 
     @GetMapping("/{bookingId}")
-    public ResponseEntity<Set<BookingDTO>> getBookingsById(){
+    public ResponseEntity<BookingDTO> getBookingById(@PathVariable Long bookingId) throws Exception {
 
 
-        List<Booking> bookings = bookingService.getBookingsBySalon(1L);
+        Booking booking = bookingService.getBookingByid(bookingId);
 
-        return ResponseEntity.ok(getBookingDTOs(bookings));
+        return ResponseEntity.ok(BookingMapper.toDTO(booking));
+    }
+
+    @PutMapping("/{bookingId}/status")
+    public ResponseEntity<BookingDTO> updateBookingStatus(@PathVariable Long bookingId, @RequestParam BookingStatus status) throws Exception {
+
+
+        Booking booking = bookingService.updateBooking(bookingId,status);
+
+        return ResponseEntity.ok(BookingMapper.toDTO(booking));
+    }
+
+    @GetMapping("/slots/salon/{salonId}/date/{date}")
+    public ResponseEntity<List<BookingSlotDTO>> getBookedSlot(@PathVariable Long salonId, @RequestParam(required = false) LocalDate date) throws Exception {
+
+
+        List<Booking> bookings = bookingService.getBookingsByDate(date,salonId);
+
+        List<BookingSlotDTO> slotsDTOs = bookings.stream().map(booking ->{
+            BookingSlotDTO slotDTO = new BookingSlotDTO();
+            slotDTO.setStartTime(booking.getStartTime());
+            slotDTO.setEndTime(booking.getEndTime());
+            return slotDTO;
+        } ).collect(Collectors.toList());
+        return ResponseEntity.ok(slotsDTOs);
+    }
+
+    @GetMapping("/report")
+    public ResponseEntity<SalonReport> getSalonReport() throws Exception {
+
+
+        SalonReport report = bookingService.getSalonReport(1L);
+
+
+        return ResponseEntity.ok(report);
     }
 
 }
---
+++ Microservices/booking-microservice/src/main/java/com/salonify/booking_microservice/dto/BookingSlotDTO.java
@@ -0,0 +1,11 @@
+package com.salonify.booking_microservice.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class BookingSlotDTO {
+    private LocalDateTime startTime;
+    private LocalDateTime endTime;
+}
